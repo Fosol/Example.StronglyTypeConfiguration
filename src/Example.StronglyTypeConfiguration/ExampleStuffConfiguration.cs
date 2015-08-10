473a3133d444864b51102fdc59e3d6b18b420efc
fosol <jeremymfoster@hotmail.com>
2015-08-10 10:13:29 -0700

    Fixed arrays.

--- src/Example.StronglyTypeConfiguration/ExampleStuffConfiguration.cs
+++ src/Example.StronglyTypeConfiguration/ExampleStuffConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class ExampleStuffConfiguration
     {
-        public string Prop1 { get; set; }
-        public string Prop2 { get; set; }
+        public string[] Prop1 { get; set; }
+        public string[] Prop2 { get; set; }
     }
 }
